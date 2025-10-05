c254fee01846a327d2401b92f090ac535ef6a91b
Hoang-hai-yen <23521847@gm.uit.edu.vn>
2025-10-05 23:17:18 +0700

    Add moving obstacle and replay element

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,10 +1,15 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
-    public TMP_Text cheeseText;
+
+    [Header("UI")]
+    public TextMeshProUGUI cheeseText;           // Text hiển thị khi đang chơi
+    public TextMeshProUGUI finalCheeseText;      // Text hiển thị ở GameOverPanel (tùy chọn)
+
     private int cheeseCount = 0;
 
     void Awake()
@@ -12,9 +17,37 @@ public class ScoreManager : MonoBehaviour
         instance = this;
     }
 
+    void Start()
+    {
+        UpdateCheeseUI();
+    }
+
     public void AddCheese(int amount)
     {
         cheeseCount += amount;
-        cheeseText.text = "Cheese: " + cheeseCount;
+        UpdateCheeseUI();
+    }
+
+    private void UpdateCheeseUI()
+    {
+        if (cheeseText != null)
+            cheeseText.text = "Cheese: " + cheeseCount;
+    }
+
+    public void ShowFinalScore()
+    {
+        if (finalCheeseText != null)
+            finalCheeseText.text = "Total Cheese: " + cheeseCount;
+    }
+
+    public void ResetScore()
+    {
+        cheeseCount = 0;
+        UpdateCheeseUI();
+    }
+
+    public int GetScore()
+    {
+        return cheeseCount;
     }
 }
